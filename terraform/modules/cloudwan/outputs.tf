4f2b9d5b9343a4578b326e291fd0ffe49ea0c027
iracic82 <iracic82@gmail.com>
2025-09-12 19:35:08 +0200

    Add output for policy ID in cloudwan module

--- terraform/modules/cloudwan/outputs.tf
+++ terraform/modules/cloudwan/outputs.tf
@@ -2,3 +2,6 @@ output "core_network_arn" {
   value = aws_networkmanager_core_network.core.arn
 }
 # No outputs; the module attaches VPCs and propagates routes
+output "policy_id" {
+  value = aws_networkmanager_core_network_policy_attachment.policy.id
+}
