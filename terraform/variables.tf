b29faa3b384d60303f7d28c5a8b03408df712ee7
iracic82 <iracic82@gmail.com>
2025-09-13 13:36:15 +0200

    Define shared VPC and spoke VPC variables
    Added variables for shared VPC and spoke VPC configurations.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,30 @@
+variable "shared_vpc" {
+  description = "Shared VPC config (NIOS-X + GM)"
+  type = object({
+    name    = string
+    cidr    = string
+    subnet  = string
+    az      = string
+    gm_ip   = string
+    key_name = string
+    nios_ip = string
+  })
+}
+
+variable "spokes" {
+  description = "Map of spoke VPCs"
+  type = map(object({
+    name       = string
+    region     = string
+    cidr       = string
+    subnet     = string
+    az         = string
+    private_ip = string
+    instance   = string
+    key_name = string
+  }))
+}
+
 variable "infoblox_join_token" {
   description = "Join token for Infoblox NIOS-X"
   type        = string
@@ -16,4 +43,3 @@ variable "tags" {
     ResourceOwner = "iracic@infoblox.com"
   }
 }
-
