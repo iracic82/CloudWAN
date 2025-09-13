9c4338d79b15ac1d8345f938fba22d73f3232078
iracic82 <iracic82@gmail.com>
2025-09-13 13:39:13 +0200

    Add BGP configs output for Connect peers
    Added output for BGP configurations of Connect peers.

--- terraform/modules/cloudwan/outputs.tf
+++ terraform/modules/cloudwan/outputs.tf
@@ -5,3 +5,20 @@ output "core_network_arn" {
 output "policy_id" {
   value = aws_networkmanager_core_network_policy_attachment.policy.id
 }
+output "connect_peer_bgp" {
+  description = "BGP configs for Connect peers (mapped to NIOSX1 / NIOSX2)"
+  value = {
+    niosx1 = {
+      aws_ip  = aws_networkmanager_connect_peer.shared_peer_1.configuration[0].bgp_configurations[0].core_network_address
+      nios_ip = aws_networkmanager_connect_peer.shared_peer_1.configuration[0].bgp_configurations[0].peer_address
+      aws_asn = aws_networkmanager_connect_peer.shared_peer_1.configuration[0].bgp_configurations[0].core_network_asn
+      nios_asn= aws_networkmanager_connect_peer.shared_peer_1.configuration[0].bgp_configurations[0].peer_asn
+    }
+    niosx2 = {
+      aws_ip  = aws_networkmanager_connect_peer.shared_peer_2.configuration[0].bgp_configurations[0].core_network_address
+      nios_ip = aws_networkmanager_connect_peer.shared_peer_2.configuration[0].bgp_configurations[0].peer_address
+      aws_asn = aws_networkmanager_connect_peer.shared_peer_2.configuration[0].bgp_configurations[0].core_network_asn
+      nios_asn= aws_networkmanager_connect_peer.shared_peer_2.configuration[0].bgp_configurations[0].peer_asn
+    }
+  }
+}
