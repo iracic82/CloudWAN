1f3d3709210d02c43f1dc6782b1e90a03abb053a
iracic82 <iracic82@gmail.com>
2025-09-13 13:42:00 +0200

    Enhance output definitions with descriptions
    Added descriptions to outputs for better clarity.

--- terraform/modules/spoke-vpc/outputs.tf
+++ terraform/modules/spoke-vpc/outputs.tf
@@ -1,26 +1,32 @@
 output "vpc_id" {
-  value = aws_vpc.spoke.id
+  value       = aws_vpc.spoke.id
+  description = "Spoke VPC ID"
 }
 
 output "subnet_arns" {
-  value = [aws_subnet.subnet.arn]
+  value       = [aws_subnet.subnet.arn]
+  description = "Subnet ARN(s)"
 }
+
 output "route_table_id" {
-  description = "ID of the default route table used by this VPC"
   value       = aws_default_route_table.this.id
+  description = "Default route table ID"
 }
 
 output "aws_vpc_cidr" {
-  value = var.aws_vpc_cidr
+  value       = aws_vpc.spoke.cidr_block
+  description = "VPC CIDR"
+}
+output "vpc_cidr" {
+  value = var.vpc_cidr
 }
 
-
 output "public_ip" {
-  description = "Public EIP of the spoke EC2 instance"
   value       = aws_eip.eip.public_ip
+  description = "Public IP of spoke EC2"
 }
 
 output "ssh_access" {
-  description = "SSH command for this spoke EC2"
-  value       = "ssh -i ${aws_key_pair.demo_key_pair.key_name}.pem ec2-user@${aws_eip.eip.public_ip}"
+  value       = "ssh -i ${var.key_name}.pem ec2-user@${aws_eip.eip.public_ip}"
+  description = "SSH command to access the spoke EC2"
 }
