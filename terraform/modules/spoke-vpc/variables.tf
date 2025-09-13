1058102bbe7a9f9ef54f07aee8fb5ae5030e5e06
iracic82 <iracic82@gmail.com>
2025-09-13 13:42:18 +0200

    Enhance variable definitions in spoke VPC module
    Added descriptions and types for VPC and subnet variables in the spoke VPC module.

--- terraform/modules/spoke-vpc/variables.tf
+++ terraform/modules/spoke-vpc/variables.tf
@@ -1,11 +1,37 @@
-variable "aws_region" {}
-variable "vpc_name" {}
-variable "aws_vpc_cidr" {}
-variable "aws_subnet_cidr" {}
-variable "az" {}
-variable "private_ip" {}
-variable "instance_name" {}
-variable "key_name" {}
+variable "vpc_name" {
+  description = "Name for the spoke VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for the spoke VPC"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "CIDR for the public subnet"
+  type        = string
+}
+
+variable "az" {
+  description = "Availability Zone"
+  type        = string
+}
+
+variable "private_ip" {
+  description = "Static private IP for the EC2 instance"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name for the EC2 instance"
+  type        = string
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+}
 variable "tags" {
   type = map(string)
   default = {
